e0a561c71777446c1491fd1b3538622925f0403d
LukasWyver <lucas_sin5@hotmail.com>
2021-03-02 23:27:34 -0300

    commit minutes reflesh

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -23,7 +23,7 @@ export function CountdownProvider({children}:CountdownProviderProps) {
 
     const { startNewChallenge } = useContext(ChallengesContext);
 
-    const [time, setTime] = useState(0.1 * 60);
+    const [time, setTime] = useState(25 * 60);
     // pegando o valor de time = 25 minutos
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
@@ -41,7 +41,7 @@ export function CountdownProvider({children}:CountdownProviderProps) {
         clearTimeout(countdownTimeout)
         setIsActive(false);
         setHasFinished(false);
-        setTime(0.1 * 60);
+        setTime(25 * 60);
     }
 
     useEffect(() => {
